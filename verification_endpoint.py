d3e95cc053d5d88298a88ae6652a69659cbb3f1a
Karen Li <77417225+karenwli0@users.noreply.github.com>
2022-03-18 23:59:11 -0700

    Add files via upload

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -15,29 +15,34 @@ def verify():
 
     payload = content.get('payload')
     platform = payload.get('platform')
+    signature = content.get('sig')
+    pk = payload.get('pk')
 
     # print(json.dumps(content, indent=1))
 
     result = False
 
     if platform == 'Ethereum':
-        eth_account.Account.enable_unaudited_hdwallet_features()
-        acct, mnemonic = eth_account.Account.create_with_mnemonic()
+        # eth_account.Account.enable_unaudited_hdwallet_features()
+        # acct, mnemonic = eth_account.Account.create_with_mnemonic()
+        #
+        # eth_pk = acct.address
+        # eth_sk = acct.key
+        #
+        # eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        # eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg, eth_sk)
 
-        eth_pk = acct.address
-        eth_sk = acct.key
+        eth_encoded_msg = payload.get('message')
 
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg, eth_sk)
-
-        if eth_account.Account.recover_message(eth_encoded_msg, signature=eth_sig_obj.signature.hex()) == eth_pk:
+        if eth_account.Account.recover_message(eth_encoded_msg, signature.hex()) == pk:
             result = True
 
     if platform == 'Algorand':
-        algo_sk, algo_pk = algosdk.account.generate_account()
-        algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'), algo_sk)
+        # algo_sk, algo_pk = algosdk.account.generate_account()
+        # algo_sig_str = algosdk.util.sign_bytes(payload.encode('utf-8'), algo_sk)
+        encoded_msg = payload.get('message')
 
-        if algosdk.util.verify_bytes(payload.encode('utf-8'), algo_sig_str, algo_pk):
+        if algosdk.util.verify_bytes(encoded_msg.encode('utf-8'), signature, pk):
             result = True
 
     # Check if signature is valid
